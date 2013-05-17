67bcfa1159585637a410d7989d91945d1e5d169f
Andrew Yates <andrewyates.name@gmail.com>
2013-05-17 00:25:14 -0400

    rm files

--- entrez2sym.R
+++
@@ -1,6 +0,0 @@
-# convert tfcat entrezids to gene symbols
-library(org.Hs.eg.db)
-entrezIDs <- as.character(read.table("tfcat.entrez.txt")[,1])
-syms <- mget(entrezIDs, org.Hs.egSYMBOL, ifnotfound=NA)
-print(sum(is.na(syms))) #1437 ok
-writeLines(na.omit(syms), "tfcat.sym.txt")
