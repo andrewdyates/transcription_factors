b623b095c5a66a7613e22600e246e94112f759f7
Andrew D. Yates <andrewyates.name@gmail.com>
2013-05-22 17:56:10 -0400

    clean up files

--- create_tf_adjm.py
+++ create_tf_adjm.py
@@ -1,49 +1,6 @@
 #!/usr/bin/python
-"""Load GSEA msigdb, nature methods list, output adj matrix
-
-# Transcription factors: 235
-# Unique Genes (plus transcription factor genes): 12280
-# 1876 transcription factors
----
-# interactions: 135831
-# genesXtf size: (12955, 1917)
+"""Load GSEA msigdb, nature methods list, output adj matrix.
+Use new symbol mapped lists.
 """
-import sys
-import gsea_msigdb.read_tf2targs as gsea
-import numpy as np
-import matrix_io as mio
-
-#GSEA_FNAME = "gsea_msigdb/tf2targs.tab"
-GSEA_FNAME = "gsea_msigdb/tf2targs.tab.aliasmap.tab"
-#NATURE_FNAME = "nature.tfsyms.txt"
-NATURE_FNAME = "nature.tfsyms.txt.aliasmap.txt"
-
-#FNAME_OUT = "/nfs/01/osu6683/tftargets/tf_adj_matrix.tab"
-FNAME_OUT = "/nfs/01/osu6683/tftargets/tf_adj_matrix_aliasmapped.tab"
-
-def main():
-  genes, tfs = set(), set()
-  adj_d = gsea.parse(GSEA_FNAME)
-  nature_g = [s.strip() for s in open(NATURE_FNAME)]
-  genes.update(nature_g)
-  tfs.update(nature_g)
-  
-  for tf, targs in adj_d.items():
-    genes.add(tf); tfs.add(tf); genes.update(targs); 
-
-  rownames = sorted(['"%s"'%s for s in genes])
-  colnames = sorted(['"%s"'%s for s in tfs])
-  row_idx = dict(((s,i) for i,s in enumerate(rownames)))
-  A = np.zeros((len(rownames), len(colnames)))
-  
-  for j,tf in enumerate(colnames):
-    for g in adj_d.get(tf,[]):
-      i = row_idx[g]
-      A[i,j] = 1
-
-  print >>sys.stderr, "# interactions: %d" % np.sum(A)
-  print >>sys.stderr, "# genesXtf size:", A.shape
-  mio.save(A, fp=FNAME_OUT, row_ids=rownames, col_ids=colnames, fmt="%d")
-
-if __name__ == "__main__":
-  main()
+tf2targs.may-21-2013.csv
+gsea_msigdb
--- gsea_msigdb/readme.txt
+++ gsea_msigdb/readme.txt
@@ -1,10 +1,19 @@
 downloaded may 15 2013
 http://www.broadinstitute.org/gsea/downloads.jsp
 
-note:
+NOTE:
 c3.tft.v3.1.entrez.gmt
 c3.tft.v3.1.symbols.gmt
-must be downloaded from GSEA website
+must be downloaded from GSEA website at http://www.broadinstitute.org/gsea/downloads.jsp
 
 contents of gsea_html can be retrieved using
 scrape_transfac.py
+
+USE: tf2targs.may-21-2013.csv
+FORMAT: TF_gene,target_gene_1,target_gene_2...
+GENERATED USING: tfgene2targetgene.py
+
+OTHER FILES:
+transfac_id_to_genes_raw.tab: compiled transfac_id to putative transcription factor genes as parsed from the gsea website; Generated using scrape_transfac.py and parse_transfac.py
+
+c3.tft.v3.1.entrez.gmt: mapping of transfac_id to target genes as downloaded from GSEA website
