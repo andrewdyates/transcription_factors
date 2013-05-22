06a940407aa66a358ea9bdc13d00c04c0f7a5bad
Andrew D. Yates <andrewyates.name@gmail.com>
2013-05-22 18:22:42 -0400

    removed old files, regenerated tf-target list, adj matrix using remapped symbols

--- create_tf_adjm.py
+++ create_tf_adjm.py
@@ -1,6 +1,68 @@
 #!/usr/bin/python
 """Load GSEA msigdb, nature methods list, output adj matrix.
-Use new symbol mapped lists.
+Use new symbol mapped lists. Use only "high quality" TF list.
+Also output csv of target to putative mappings.
+
+python create_tf_adjm.py 
+# genes total: 12609
+# transcription factors: 1370
+# transcription factors with targets: 234
+Printing list of transcription factors with putative targets to all_tf_with_targs_may_21_2013.csv
+# interactions: 132865
+# genesXtf size: (12609, 1370)
 """
-tf2targs.may-21-2013.csv
-gsea_msigdb
+import numpy as np
+import matrix_io as mio
+import sys
+
+TARGS_FNAME = "gsea_msigdb/tf2targs.may-21-2013.csv"
+TFLIST_FNAME = "nature_census/nature_tf_list_high_may21_2013.txt"
+MERGED_LIST_OUT_FNAME = "all_tf_with_targs_may_21_2013.csv"
+# Warning: the adj matrix is a huge file. Move it out of the local directory once created.
+ADJM_OUT_FNAME = "all_tf_with_targs_may_21_2013_adjm.tab"
+
+
+def main():
+  all_genes = set()
+  tf_targs = {}
+  for line in open(TARGS_FNAME):
+    row = line.strip('\n\r').split(',')
+    tf_targs[row[0]] = row[1:]
+    all_genes.update(row[1:])
+   
+  tfs = set(tf_targs.keys())
+  for line in open(TFLIST_FNAME):
+    tfs.add(line.strip('\n\r'))
+  all_genes.update(tfs)
+
+  # STATS
+  print "# genes total:", len(all_genes)
+  print "# transcription factors:", len(tfs)
+  print "# transcription factors with targets:", len(tf_targs)
+  # Text adj list
+  print "Printing list of transcription factors with putative targets to %s" % MERGED_LIST_OUT_FNAME
+  fp_out = open(MERGED_LIST_OUT_FNAME,"w")
+  for tf in sorted(tfs):
+    if tf in tf_targs:
+      print >>fp_out, "%s,%s" % (tf,",".join(tf_targs))
+    else:
+      print >>fp_out, tf
+      
+  # Text adj matrix, columns TF, rows TF targets
+  rownames = sorted(all_genes)
+  colnames = sorted(tfs)
+  row_idx = dict(((s,i) for i,s in enumerate(rownames)))
+  A = np.zeros((len(rownames), len(colnames)))
+
+  for j,tf in enumerate(colnames):
+    for g in tf_targs.get(tf,[]):
+      i = row_idx[g]
+      A[i,j] = 1
+      
+  print >>sys.stderr, "# interactions: %d" % np.sum(A)
+  print >>sys.stderr, "# genesXtf size:", A.shape
+  mio.save(A, fp=ADJM_OUT_FNAME, row_ids=['"%s"'%s for s in rownames], col_ids=['"%s"'%s for s in colnames], fmt="%d")
+
+
+if __name__ == "__main__":
+  main()
--- gsea_msigdb/readme.txt
+++ gsea_msigdb/readme.txt
@@ -1,19 +1,14 @@
 downloaded may 15 2013
 http://www.broadinstitute.org/gsea/downloads.jsp
 
-NOTE:
-c3.tft.v3.1.entrez.gmt
-c3.tft.v3.1.symbols.gmt
-must be downloaded from GSEA website at http://www.broadinstitute.org/gsea/downloads.jsp
-
-contents of gsea_html can be retrieved using
-scrape_transfac.py
+NOTES:
++ c3.tft.v3.1.entrez.gmt must be downloaded from GSEA website at http://www.broadinstitute.org/gsea/downloads.jsp
++ HTML file contents of directory gsea_html as required by parse_transfac.py (not included in this repository due to large size) can be downloaded from the web using scrape_transfac.py.
 
 USE: tf2targs.may-21-2013.csv
 FORMAT: TF_gene,target_gene_1,target_gene_2...
 GENERATED USING: tfgene2targetgene.py
 
 OTHER FILES:
-transfac_id_to_genes_raw.tab: compiled transfac_id to putative transcription factor genes as parsed from the gsea website; Generated using scrape_transfac.py and parse_transfac.py
-
-c3.tft.v3.1.entrez.gmt: mapping of transfac_id to target genes as downloaded from GSEA website
++ transfac_id_to_genes_raw.tab: compiled transfac_id to putative transcription factor genes as parsed from the gsea website; Generated using scrape_transfac.py and parse_transfac.py
++ c3.tft.v3.1.entrez.gmt: mapping of transfac_id to target genes as downloaded from GSEA website
