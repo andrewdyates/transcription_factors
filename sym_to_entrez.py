2769d7126b91fdcdede6112a3880ae73d7fff404
Andrew Yates <andrewyates.name@gmail.com>
2013-06-12 11:25:46 -0400

    correctly handle multiple entrez ids per symbol

--- sym_to_entrez.py
+++ sym_to_entrez.py
@@ -5,22 +5,22 @@ import ncbi_gene_info
 import hugo_gene_symbols
 import sys
 H = hugo_gene_symbols.load()
-R = ncbi_gene_info.load()
+R = ncbi_gene_info.load_list()
 
 FNAME = "all_tf_with_targs_may_21_2013.csv"
 FNAME_ENTREZ = FNAME + ".entrez.csv"
-MANUAL_MAP = {'FOXM1C': '2305'}
+MANUAL_MAP = {'FOXM1C': set(['2305'])}
 
-sym_to_entrez = dict((r[1],r[0]) for r in R)
-alias_to_entrez = {}
-for r in R:
-  entrez, sym, alias_s = r
-  aliases = alias_s.split('|')
-  for a in aliases:
-    if a and a != '-':
-      alias_to_entrez.setdefault(a,set()).add(entrez)
+sym_to_entrez, alias_to_entrez = ncbi_gene_info.sym_to_entrez_dicts(R)
 
-def get_entrez(s):
+multi_sym = [(k,v) for k,v in sym_to_entrez.items() if len(v) > 1]
+multi_alias = [(k,v) for k,v in alias_to_entrez.items() if len(v) > 1]
+print "symbols with multiple entrez IDs", len(multi_sym)
+print multi_sym
+print "---"
+print "aliases with multiple entrez IDs", len(multi_alias)
+
+def get_entrezes(s):
   hugo = H.find_sym(s)
   if s in MANUAL_MAP:
     return MANUAL_MAP[s]
@@ -28,11 +28,9 @@ def get_entrez(s):
     return sym_to_entrez[s]
   elif hugo in sym_to_entrez:
     return sym_to_entrez[hugo]
-  elif s in alias_to_entrez and len(alias_to_entrez[s]) == 1:
-    z = alias_to_entrez[s].pop()
-    alias_to_entrez[s].add(z)
-    return z
-  elif hugo in alias_to_entrez and len(alias_to_entrez[hugo]) == 1:
+  elif s in alias_to_entrez:
+    return alias_to_entrez[s]
+  elif hugo in alias_to_entrez:
     return alias_to_entrez[hugo]
   else:
     return None
@@ -43,19 +41,23 @@ fp_out = open(FNAME_ENTREZ, 'w')
 for line in open(FNAME):
   row = line.strip('\n\r').split(',')
   tf, targs = row[0], row[1:]
-  tfe = get_entrez(tf)
-  rowe = [tfe]
+  tfe = get_entrezes(tf)
   if not tfe:
-    print >>sys.stderr, "Cannot find entrez ID for TF symbol %s" % tf
+    print >>sys.stderr, "Cannot find any entrez ID for TF symbol %s" % tf
     no_entrez.add(tf)
     continue
 
-  for ss in targs:
-    sse = get_entrez(ss)
-    if not sse:
-      print >>sys.stderr, "Cannot find entrez ID for tf %s target symbol %s" % (tf, ss)
-      no_entrez.add(ss)
-    else:
-      rowe.append(sse)
-  print >>fp_out, ",".join(rowe)
+  if len(tfe) > 1:
+    print >>sys.stderr, "TF %s has %d entrez IDs: %s" % (tf, len(tfe), ", ".join(tfe))
+  for tfei in tfe:
+    rowe = [tfei]
+    for ss in targs:
+      sse = get_entrezes(ss)
+      if not sse:
+        print >>sys.stderr, "Cannot find any entrez ID for tf %s target symbol %s" % (tf, ss)
+        no_entrez.add(ss)
+      else:
+        for sss in sse:
+          rowe.append(sss)
+    print >>fp_out, ",".join(rowe)
   
